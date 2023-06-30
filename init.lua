abb4d1c387d9e2cfe684f4d2d02a89a635267a42
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-07-01 01:11:46 +0500

    remove debug cmd and improve params check

--- init.lua
+++ init.lua
@@ -1,7 +1,10 @@
 local http = core.request_http_api()
 
 function webimage(url, callback)
-	if not url then
+	if type(callback) ~= "function" then
+		return
+	end
+	if type(url) ~= "string" then
 		callback("")
 		return
 	end
@@ -20,10 +23,3 @@ function webimage(url, callback)
 		end
 	)
 end
-
-core.register_chatcommand("http",{
-  func = function(name,param)
-	webimage(param, function(img)
-			core.show_formspec(name, "test", "size[16,9]image[1,1;8,8;" .. img .. "]")
-	end)
-end})
